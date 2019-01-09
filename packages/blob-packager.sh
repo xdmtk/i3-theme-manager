f1489888831d40cebd9c5723d4d40334f5b5b2df
Nick Martinez <nick@xdmtk.org>
2019-01-09 15:33:03 -0800

    sep python

--- packages/blob-packager.sh
+++ packages/blob-packager.sh
@@ -1,64 +1,100 @@
 #!/bin/bash
-get_openbox_theme=<<END 
-import os
-import subprocess
-ll = []
-set = False
-if os.path.isfile("rc.xml"):
-	with open("rc.xml", "r") as f:
-		for line in f:
-			if line.find("<theme>") != -1:
-				set = True
-			elif line.find("</theme>") != -1:
-				set = False
-			if set is True:
-				ll.append(line)
-	theme = None
-	for line in ll:
-		if line.find("<name>") != -1:
-			theme = line[line.find("<name>")+6:line.find("</name")]
-			break
-	print(theme)
-END
-		
-get_wallpapers=<<END 
-import os
-import subprocess
-if os.path.isfile("bg-saved.cfg"):
-	with open("bg-saved.cfg", "r") as f:
-		for line in f:
-			if line.find("file=") != -1:
-				wp = line[line.find("="):]
-				subprocess.call(["cp", wp, "wallpapers/"])
-END
+package() {
 
-gtk_get_theme=<<END
-with open("settings.ini", "r") as f:
-	for line in f:
-		if line.find("gtk-theme-name") != -1:
-			l = line.split("=")
-			print(l[1])
-			break
-END
-
-gtk_get_icons=<<END
-with open("settings.ini", "r") as f:
-	for line in f:
-		if line.find("gtk-icon-theme-name") != -1:
-			l = line.split("=")
-			print(l[1])
-			break
-END
+	getters="$(pwd)/getters.py"
+	echo "1"
+	# Set base location
+	cd ~
+	if [ ! -e "~/tmp_dir" ] ; then
+		mkdir tmp_dir
+		cd tmp_dir
+		base_loc=$(pwd)
+	else 
+		echo "what is in tmp_dir?"
+		exit
+	fi
+	
+	
+	echo "2"
+	# Nitrogen
+	cp -R ~/.config/nitrogen . 
+	cd nitrogen
+	mkdir wallpapers
+	python $getters -gwp
 
 
-gtk_get_cursor=<<END
-with open("settings.ini", "r") as f:
-	for line in f:
-		if line.find("gtk-cursor-theme-name") != -1:
-			l = line.split("=")
-			print(l[1])
-			break
-END
+	# Terminator
+	cd $base_loc
+	cp -R ~/.config/terminator .
+
+	echo "3"
+	
+	# Openbox
+	cp -R ~/.config/openbox .
+
+	cd openbox
+	ob_theme=$(python $getters -gob)
+	extractThemes "openbox" $ob_theme "themes"
+
+	echo "4"
+
+
+
+	#GTK 3.0
+	cd $base_loc
+	cp -R ~/.config/gtk-3.0 .
+	cd gtk-3.0
+	echo "5"
+	return_loc=$(pwd)
+
+
+	gtk_theme=$(python $getters -gtkth)
+	extractThemes "gtk-3.0" $gtk_theme "themes" $return_loc
+	echo "6"
+
+	gtk_icons=$(python $getters -gtkic)
+	extractThemes "gtk-3.0" $gtk_icons "icons" $return_loc
+	echo "7"
+
+	gtk_cursor=$(python $getters -gtkcur)
+	extractThemes "gtk-3.0" $gtk_cursor "icons" $return_loc
+	echo "8"
+
+
+}
+
+function extractThemes() {
+	target_prog="${1}"
+	target_theme="${2}"
+	target_type="${3}"
+	echo $1 $2 $3	
+	current_theme=${target_theme}
+	if [ -d "/home/${USER}/.themes" ] ; then
+		echo "11"
+		cd ~/.themes
+		if [ -d $current_theme ] ; then
+			cp -R $current_theme "${base_loc}/${target_prog}"
+		else
+			echo "44"
+			cd /usr/share/${target_type}
+			if [ -e $current_theme ] ; then
+				cp -R $current_theme "${base_loc}/${target_prog}"
+			else
+				echo "couldn't find current ${target_prog} theme: ${current_theme} where did you put it?"
+			fi
+		fi
+	else
+		cd /usr/share/${target_type}
+		if [ -e $current_theme ] ; then
+			cp -R $current_theme "${base_loc}/${target_prog}"
+		else
+			echo "couldn't find current ${target_prog} theme: ${current_theme} where did you put it?"
+		fi
+	fi
+	cd $4
+}
+
+
 
 
 
@@ -92,78 +128,8 @@ usage: blob-packager.sh [ package/load ]
 	-l : Loads a packaged theme
 "
 elif [ "$switch" = "-p" ] ; then
-	package()
+	package
 elif [ "$switch" = "-l" ] ; then
-	load()
+	load
 fi
 
-package() {
-	
-	# Set base location
-	cd ~
-	if [ ! -e "~/tmp_dir" ] ; then
-		mkdir tmp_dir
-		cd tmp_dir
-		base_loc=$(pwd)
-		mkdir nitrogen
-		cd nitrogen
-		mkdir wallpapers
-	else 
-		echo "what is in tmp_dir?"
-		exit
-	fi
-	
-	
-	# Nitrogen
-	cp -R ~/.config/nitrogen . 
-	python - $get_wallpapers
-
-	# Terminator
-	cd $base_loc
-	cp -R ~/.config/terminator .
-
-	
-	# Openbox
-	cp -R ~/.config/openbox .
-
-	cd openbox
-	mkdir cur_theme
-	ob_theme=$(python - $get_openbox_theme)
-	extractThemes("openbox", $ob_theme)
-
-
-
-
-	#GTK 3.0
-	cd $base_loc
-	cp -R ~/.config/gtk-3.0 .
-	cd gtk-3.0
-	gtk_theme=$gtk_get_theme
-	gtk_icons=$gtk_get_icons
-	gtk_cursor=$gtk_get_cursor
-}
-
-function extractThemes(target_prog, target_theme) {
-
-	current_theme=${target_theme}
-	if [ -e "~/.themes " ] ; then
-		cd ~/.themes
-		if [ -e $current_theme ] ; then
-			cp -R $current_theme "${base_loc}/${target_prog}"
-		else
-			cd /usr/share/themes
-			if [ -e $current_theme ] ; then
-				cp -R $current_theme "${base_loc}/${target_prog}"
-			else
-				echo "couldn't find current ${target_prog} theme, where did you put it?"
-			fi
-		fi
-	else
-		cd /usr/share/themes
-		if [ -e $current_theme ] ; then
-			cp -R $current_theme "${base_loc}/${target_prog}"
-		else
-			echo "couldn't find current ${target_prog} theme, where did you put it?"
-		fi
-	fi
-}
---
+++ packages/getters.py
@@ -0,0 +1,81 @@
+import os
+import subprocess
+import sys
+import pdb
+def main():
+	if (sys.argv[1] == "-gob"):
+		get_openbox_theme()
+	elif (sys.argv[1] == "-gwp"):
+		get_wallpapers()
+	elif (sys.argv[1] == "-gtkth"):
+		gtk_get_theme()
+	elif (sys.argv[1] == "-gtkic"):
+		gtk_get_icons()
+	elif (sys.argv[1] == "-gtkcur"):
+		gtk_get_cursor()
+	
+
+def get_openbox_theme():
+	ll = []
+	set = False
+	if os.path.isfile("rc.xml"):
+		with open("rc.xml", "r") as f:
+			for line in f:
+				if line.find("<theme>") != -1:
+					set = True
+				elif line.find("</theme>") != -1:
+					set = False
+				if set is True:
+					ll.append(line)
+		theme = None
+		for line in ll:
+			if line.find("<name>") != -1:
+				theme = line[line.find("<name>")+6:line.find("</name")]
+				break
+		print(theme)
+	quit()
+
+		
+def get_wallpapers():
+	if os.path.isfile("bg-saved.cfg"):
+		with open("bg-saved.cfg", "r") as f:
+			for line in f:
+				if line.find("file=") != -1:
+					wp = line[line.find("=")+1:]
+					subprocess.call(["cp", wp[:-1], "wallpapers/."])
+	quit()
+
+def gtk_get_theme():
+	with open("settings.ini", "r") as f:
+		for line in f:
+			if line.find("gtk-theme-name") != -1:
+				l = line.split("=")
+				print(l[1])
+				break
+	quit()
+
+def gtk_get_icons():
+	with open("settings.ini", "r") as f:
+		for line in f:
+			if line.find("gtk-icon-theme-name") != -1:
+				l = line.split("=")
+				print(l[1])
+				break
+	quit()
+
+
+def gtk_get_cursor():
+	with open("settings.ini", "r") as f:
+		for line in f:
+			if line.find("gtk-cursor-theme-name") != -1:
+				l = line.split("=")
+				print(l[1])
+				break
+	quit()
+
+
+
+
+
+
+main()
