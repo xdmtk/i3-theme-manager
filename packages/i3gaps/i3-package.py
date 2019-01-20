9c147b60a1e3052f2fc5ef4e5af9f626c0b8b8b8
Nick Martinez <nick@xdmtk.org>
2019-01-20 00:19:06 -0800

    Start config setup

--- packages/i3gaps/i3-package.py
+++ packages/i3gaps/i3-package.py
@@ -4,17 +4,34 @@ import sys
 
 MODE_PACKAGE = False
 MODE_LOAD = False
+MODE_CONFIG = False
 ARG_FAIL = -1
 
 def main():
     if (parse_args() == ARG_FAIL):
         show_usage()
+        quit()
+    
+    if MODE_PACKAGE is True:
+        package
+
 
 
 
 
 def parse_args():
     global MODE_PACKAGE ; global MODE_LOAD
+
+    # Check for config settings
+    USER_HOME = 'home/' + os.getenv('USER') + '/'
+    if not os.path.isdir(USER_HOME + '.config/i3packager'):
+        os.mkdir(USER_HOME + '.config/i3packager')
+        MODE_CONFIG = True
+        print('[+] Configuration files not found... entering config mode')
+        return
+
+
+
     if sys.argv > 1:
         for arg in sys.argv:
             if arg.find("p") != -1:
@@ -23,7 +40,28 @@ def parse_args():
             elif arg.find("l") != -1:
                 MODE_PACKAGE = True
                 return
+            elif arg.find("c") != -1:
+                MODE_CONFIG = True
+                return
             else
                 return ARG_FAIL 
+
+    return ARG_FAIL
+
+def show_usage():
+    usage = '''
+    
+    * * * * * * * * * * * * *  i3-Gaps Theme Packaging Script * * * * * * * * * 
+
+        usage: python i3-package.py [ -l (load) / -p (package) / -c (config) ] 
+
+            For first use, run with arg -c to configure theme directories
+'''
+    print(usage)
+        
+    
+
+
+
 
 
