ac4954950491c6618988acd5307eef9f26af5b34
Nick Martinez <nick@xdmtk.org>
2019-02-01 16:25:24 -0800

    Add delete button

--- i3_qt.py
+++ i3_qt.py
@@ -21,7 +21,7 @@ class Ui_MainWindow(object):
         MAIN_WIN_GLOB = MainWindow
         MainWindow.setObjectName("MainWindow")
         MainWindow.setWindowModality(QtCore.Qt.ApplicationModal)
-        MainWindow.resize(848, 766)
+        MainWindow.resize(848, 796)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -30,7 +30,7 @@ class Ui_MainWindow(object):
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.theme_list = QtWidgets.QListWidget(self.centralWidget)
-        self.theme_list.setGeometry(QtCore.QRect(10, 660, 691, 91))
+        self.theme_list.setGeometry(QtCore.QRect(10, 660, 691, 121))
         self.theme_list.setObjectName("theme_list")
         self.theme_view = QtWidgets.QLabel(self.centralWidget)
         self.theme_view.setGeometry(QtCore.QRect(10, 10, 831, 631))
@@ -38,12 +38,18 @@ class Ui_MainWindow(object):
         self.load_button = QtWidgets.QPushButton(self.centralWidget)
         self.load_button.setGeometry(QtCore.QRect(710, 660, 131, 31))
         self.load_button.setObjectName("load_button")
+        self.load_button.setEnabled(False)
         self.package_button = QtWidgets.QPushButton(self.centralWidget)
         self.package_button.setGeometry(QtCore.QRect(710, 690, 131, 31))
         self.package_button.setObjectName("package_button")
         self.revert_button = QtWidgets.QPushButton(self.centralWidget)
         self.revert_button.setGeometry(QtCore.QRect(710, 720, 131, 31))
         self.revert_button.setObjectName("revert_button")
+        self.revert_button.setEnabled(False)
+        self.delete_button = QtWidgets.QPushButton(self.centralWidget)
+        self.delete_button.setGeometry(QtCore.QRect(710, 750, 131, 31))
+        self.delete_button.setObjectName("delete_button")
+        self.delete_button.setEnabled(False)
         MainWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(MainWindow)
@@ -51,6 +57,7 @@ class Ui_MainWindow(object):
         self.theme_list.itemClicked.connect(self.onclick_theme_list)
         self.load_button.clicked.connect(self.onclick_load_button)
         self.package_button.clicked.connect(self.onclick_package_button)
+        self.delete_button.clicked.connect(self.onclick_delete_button)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -58,6 +65,7 @@ class Ui_MainWindow(object):
         self.load_button.setText(_translate("MainWindow", "&Load"))
         self.package_button.setText(_translate("MainWindow", "&Package"))
         self.revert_button.setText(_translate("MainWindow", "&Revert"))
+        self.delete_button.setText(_translate("MainWindow", "&Delete"))
 
 
 
@@ -75,6 +83,9 @@ class Ui_MainWindow(object):
             if os.path.isdir(I3P_DIR + dir) and dir != ".last":
                 self.theme_list.addItem(dir)
 
+        for dir in theme_dir_listing:
+            if dir == ".last":
+                self.revert_button.setEnabled(True)
 
     def onclick_load_button(self):
         if CURRENT_SELECTION is None or CURRENT_SELECTION == "":
@@ -118,7 +129,8 @@ class Ui_MainWindow(object):
         global CURRENT_SELECTION
         selected_item = self.theme_list.selectedItems()[0]
         CURRENT_SELECTION = selected_item.text()
-
+        self.load_button.setEnabled(True)
+        self.delete_button.setEnabled(True)
 
         # Quick and dirty...
         USER = os.environ.get("USER")
@@ -127,6 +139,8 @@ class Ui_MainWindow(object):
         image = QtGui.QPixmap.fromImage(image)
         self.theme_view.setPixmap(image.scaled(self.theme_view.width(),self.theme_view.height()))
 
+    def onclick_delete_button(self):
+        pass
 
 if __name__ == "__main__":
     import sys
